bbf4665bb58fbf830e3a502fb715f5a74c6d0604
Alfie Lamerton <46544750+alamerton@users.noreply.github.com>
2023-05-15 11:00:48 +0100

    Added post like test and forcexit

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,15 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import likeRoutes from "./routes/Like.route";
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use("/", likeRoutes);
+const server = express();
+server.use(cors());
+server.use(bodyParser.json());
+server.use("/", likeRoutes);
 
 const port = 5002;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+export default server
